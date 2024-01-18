1c44ed47134abd3f373fd5d0ab6fc1f6faa061c6
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-01-18 11:59:12 +0100

    update commit

--- src/Animale.java
+++ src/Animale.java
@@ -1,2 +1,7 @@
 public class Animale {
+
+    @Override
+    public String toString() {
+        return "il verso dell'animale è";
+    }
 }
--- src/Gatto.java
+++ src/Gatto.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Gatto {
+public class Gatto extends Animale{
+
+
+    @Override
+    public java.lang.String toString() {
+        return "Miao Miao";
+    }
 }
